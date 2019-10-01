15a9fad93a4a51e5761a19f9744968c0ad622c7e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-10-01 02:39:21 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ const path = require( 'path' );
 var chai = require( 'chai' );
 chai.use( require( 'chai-fs' ) );
 
-describe( 'this lab', () => {
+// describe( 'this lab', () => {
 
 //   it( 'has a folder named my-repository', () => {
 //     chai.assert.isDirectory( './my-repository', 'no folder name "my-repository" was found' );
